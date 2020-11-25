576eb1baddb95756229cac6ad782347193bb124f
imadcollin <imad.collin@gmail.com>
2020-11-25 11:49:39 +0100

    Add seeds and show data

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {useState, useEffect} from 'react';
+import React, {PropTypes, Component} from 'react';
+
+import {View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './Components/Header';
+import {itemList} from './Seeds/MockData';
+import ListItem from './Components/ListItem';
 const App = () => {
+  const [item, setItem] = useState(null);
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    const getItems = async () => {
+      await setItems(itemList);
+    };
+    getItems();
+  }, []);
   return (
     <View style={styles.root}>
       <Header title="Shopping List"></Header>
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem item={item}></ListItem>}></FlatList>
     </View>
   );
 };
---
+++ Seeds/MockData.js
@@ -0,0 +1,19 @@
+export const itemList = [
+  {
+    id: 1,
+    name: 'item 1 ',
+    price: 30,
+  },
+  {
+    id: 2,
+    name: 'item 3 ',
+    price: 50,
+  },
+  {
+    id: 3,
+    name: 'item 3 ',
+    price: 60,
+  },
+];
+
+export default {itemList};
